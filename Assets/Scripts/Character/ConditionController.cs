2a6394cff484edeb5df1897a7db21b963c84e384
choijiho0925 <jiho.0925.choi@gmail.com>
2025-06-11 09:33:00 +0900

    Refactor : 줄바꿈

--- Assets/Scripts/Character/ConditionController.cs
+++ Assets/Scripts/Character/ConditionController.cs
@@ -8,47 +8,47 @@ public class ConditionController : MonoBehaviour
     public void CheckItem()
     {
         if (beforeItem != null)
-    {
-        switch (beforeItem.equipData[0].type)
         {
-            case EquipTpye.Health: 
-                    GameManager.Instance.Player.condition.Health -= (int)beforeItem.equipData[0].value; 
-            break;
-            case EquipTpye.ReactionSpeed: 
-                    GameManager.Instance.Player.condition.ReactionSpeed -= (int)beforeItem.equipData[0].value; 
-            break;
-            case EquipTpye.IQ: 
-                    GameManager.Instance.Player.condition.IQLevel -= (int)beforeItem.equipData[0].value;
-            break;
-            case EquipTpye.CodingPower: 
-                    GameManager.Instance.Player.condition.CodingPower -= (int)beforeItem.equipData[0].value; 
-            break;
-        }
-    }
-
-    if (equipItem != null)
-    {
-        switch (equipItem.equipData[0].type)
-        {
-            case EquipTpye.Health: 
-                    GameManager.Instance.Player.condition.Health += (int)equipItem.equipData[0].value; 
-            break;
-            case EquipTpye.ReactionSpeed: 
-                    GameManager.Instance.Player.condition.ReactionSpeed += (int)equipItem.equipData[0].value; 
-            break;
-            case EquipTpye.IQ: 
-                    GameManager.Instance.Player.condition.IQLevel += (int)equipItem.equipData[0].value; 
-            break;
-            case EquipTpye.CodingPower: 
-                    GameManager.Instance.Player.condition.CodingPower += (int)equipItem.equipData[0].value; 
-            break;
+            switch (beforeItem.equipData[0].type)
+            {
+                case EquipTpye.Health: 
+                        GameManager.Instance.Player.condition.Health -= (int)beforeItem.equipData[0].value; 
+                break;
+                case EquipTpye.ReactionSpeed: 
+                        GameManager.Instance.Player.condition.ReactionSpeed -= (int)beforeItem.equipData[0].value; 
+                break;
+                case EquipTpye.IQ: 
+                        GameManager.Instance.Player.condition.IQLevel -= (int)beforeItem.equipData[0].value;
+                break;
+                case EquipTpye.CodingPower: 
+                        GameManager.Instance.Player.condition.CodingPower -= (int)beforeItem.equipData[0].value; 
+                break;
+            }
         }
 
-        beforeItem = equipItem;
-    }
-    else
-    {
-        beforeItem = null;
-    }
+        if (equipItem != null)
+        {
+            switch (equipItem.equipData[0].type)
+            {
+                case EquipTpye.Health: 
+                        GameManager.Instance.Player.condition.Health += (int)equipItem.equipData[0].value; 
+                break;
+                case EquipTpye.ReactionSpeed: 
+                        GameManager.Instance.Player.condition.ReactionSpeed += (int)equipItem.equipData[0].value; 
+                break;
+                case EquipTpye.IQ: 
+                        GameManager.Instance.Player.condition.IQLevel += (int)equipItem.equipData[0].value; 
+                break;
+                case EquipTpye.CodingPower: 
+                        GameManager.Instance.Player.condition.CodingPower += (int)equipItem.equipData[0].value; 
+                break;
+            }
+
+            beforeItem = equipItem;
+        }
+        else
+        {
+            beforeItem = null;
+        }
     }
 }
